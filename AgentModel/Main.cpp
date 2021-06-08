3047cf2abc414acfb56b88eb39f07042a311e8d0
AndreyMo1seev <45105942+AndreyMo1seev@users.noreply.github.com>
2021-06-08 16:43:13 +0300

    Add testing and self isolations

--- AgentModel/Main.cpp
+++ AgentModel/Main.cpp
@@ -9,24 +9,18 @@
 #include <random>
 #include <time.h>
 #include <chrono>
-#define COEFF_OF_INFESTATIONS 190
-#define NUMBER_OF_SOCIAL_CONNECTIONS 20
-#define DURATION 60
+#include <fstream>
+//#include "../matplotlib-cpp-master/matplotlibcpp.h"
+#define COEFF_OF_INFESTATIONS 103
+#define NUMBER_OF_SOCIAL_CONNECTIONS 31
+#define DURATION 90
+#define SELF_ISOLATION_INGEX 92
+#define TEST_PROBABILITY 95
+#define START_TESTS 25
 
 using namespace std;
-/*
-bool operator!= (Agent() ag1, Agent() ag2)
-{
-	if (ag1.GetAge() != ag2.GetAge() && ag1.GetDayOfDayIllness() != ag2.GetDayOfDayIllness() && ag1.GetStage() != ag2.GetStage() && ag1.GetGender() != ag2.GetGender())
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-*/
+//namespace plt = matplotlibcpp;
+
 class Agent
 {
 private:
@@ -34,6 +28,8 @@ private:
 	int dayOfDayIllness;
 	bool infected;
 	bool death;
+	bool self_isolation;
+	bool test;
 	char stage;
 	char gender;
 
@@ -60,6 +56,21 @@ public:
 	{
 		return infected;
 	}
+	bool IsRecovered()
+	{
+		if (stage == 'r')
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	bool IsTest()
+	{
+		return test;
+	}
 	int GetAge()
 	{
 		return age;
@@ -84,6 +95,22 @@ public:
 	{
 		return death;
 	}
+	void SetIsolation()
+	{
+		self_isolation = true;
+	}
+	void MakeTest()
+	{
+		test = true;
+		if (infected == true)
+		{
+			self_isolation = true;
+		}
+	}
+	bool Isolation()
+	{
+		return self_isolation;
+	}
 	
 	bool operator!= (Agent ag)
 	{
@@ -100,7 +127,7 @@ public:
 	void ControlStage()
 	{
 		//Для начала примем, что все проходят одни и те же стадии и они длятся одно и то же время 
-		if (dayOfDayIllness > 5)
+		if (dayOfDayIllness > 3)
 		{
 			stage = 's';
 		}
@@ -131,7 +158,7 @@ public:
 		// предполагаем, что человек может умереть только на второй стадии болезни (t) и то, что эта 
 		// фаза длится примерно 10 дней. Вероятности умереть в каждый день стадии (t) равны
 		std::mt19937 death(chrono::steady_clock::now().time_since_epoch().count());
-		std::uniform_int_distribution<> deathProbability(0, 1000);
+		std::uniform_int_distribution<> deathProbability(0, 34000);
 		int probabilityValue = deathProbability(death);
 		if (age <= 10)
 		{
@@ -172,7 +199,50 @@ public:
 		std::mt19937 gend(chrono::steady_clock::now().time_since_epoch().count());
 		std::uniform_int_distribution<> uid(1, 90);
 		std::uniform_int_distribution<> uid2(0, 1);
-		age = uid(gend);
+		std::mt19937 ageDistribution(chrono::steady_clock::now().time_since_epoch().count());
+		std::uniform_int_distribution<> ageProbability(0, 10000);
+		int probabilityValue = ageProbability(ageDistribution);
+		//age = uid(gend);
+		if (probabilityValue <= 1219)
+		{
+			std::uniform_int_distribution<> uid3(0, 9);
+			age = uid3(gend);
+		}
+		if (1219 < probabilityValue <= 3367)
+		{
+			std::uniform_int_distribution<> uid3(10, 29);
+			age = uid3(gend);
+		}
+		if (3367 < probabilityValue <= 5046)
+		{
+			std::uniform_int_distribution<> uid3(30, 39);
+			age = uid3(gend);
+		}
+		if (5046 < probabilityValue <= 6450)
+		{
+			std::uniform_int_distribution<> uid3(40, 49);
+			age = uid3(gend);
+		}
+		if (6450 < probabilityValue <= 7764)
+		{
+			std::uniform_int_distribution<> uid3(50, 59);
+			age = uid3(gend);
+		}
+		if (7764 < probabilityValue <= 9021)
+		{
+			std::uniform_int_distribution<> uid3(60, 69);
+			age = uid3(gend);
+		}
+		if (9021 < probabilityValue <= 9613)
+		{
+			std::uniform_int_distribution<> uid3(70, 79);
+			age = uid3(gend);
+		}
+		if (9613 < probabilityValue <= 10000)
+		{
+			std::uniform_int_distribution<> uid3(80, 95);
+			age = uid3(gend);
+		}
 		int tmp = uid2(gend);
 			if (tmp == 0)
 			{
@@ -186,6 +256,7 @@ public:
 		stage = 'n';
 		dayOfDayIllness = -1;
 		death = false;
+		self_isolation = false;
 	}
 	~Agent()
 	{
@@ -241,33 +312,96 @@ void SimulateInfection(vector<Agent> agents, const int nubmerOfAgents)
 {
 	int counter = 1;
 	int death = 0;
+	int recovered = 0;
+	int tests = 0;
+	vector<int> listOfInfected;
+	vector<int> listOfDeaths;
+	vector<int> listOfRecovered;
+	vector<int> listofTests;
+	vector<int> days;
+	int nsc = NUMBER_OF_SOCIAL_CONNECTIONS;
+	
 	for (int i = 0; i < DURATION; i++)
 	{
+		cout << "Day" << i << endl;
+		if (i > 55) 
+		{
+			nsc = 26;
+		}
+		else
+		{
+			if (i > 80)
+			{
+				nsc = 24;
+			}
+
+		}
+
 		for (auto& a : agents)
 		{
 			if (a.IsDeath()) {
 				continue;
 			}
-			
+
+			if (a.IsRecovered())
+			{
+				recovered++;
+			}
+			/*
+			if (a.IsTest())
+			{
+				tests++;
+			}
+			*/
 			if (a.IsInfected())
 			{
 				std::mt19937 socialGroup(chrono::steady_clock::now().time_since_epoch().count());
-				std::uniform_int_distribution<> socialGroupRange(0, NUMBER_OF_SOCIAL_CONNECTIONS);
-				std::uniform_int_distribution<> position(0, nubmerOfAgents - NUMBER_OF_SOCIAL_CONNECTIONS);
+				std::uniform_int_distribution<> socialGroupRange(0, nsc);
+				std::uniform_int_distribution<> position(0, nubmerOfAgents - nsc);
 				int socialNumber = socialGroupRange(socialGroup);
 				int num = position(socialGroup);
 				a.IncrementDayOfDayIllness();
 				a.ControlStage();
-				for (int i = num; i < num + socialNumber; i++)
-				{
-					if (!agents[i].IsInfected())
+				if (!a.Isolation()) {
+					for (int i = num; i < num + socialNumber; i++)
 					{
-
-						if (ContactInfectionProbability())
+						if (!agents[i].IsInfected())
 						{
-							agents[i].Infection();
-							counter++;
+
+							if (ContactInfectionProbability())
+							{
+								agents[i].Infection();
+								counter++;
+							}
 						}
+					}
+				}
+				if (a.GetStage() == 's' and !a.Isolation())
+				{
+					if (i > START_TESTS)
+					{
+						std::uniform_int_distribution<> willmaketest(0, 100);
+						int testprob = willmaketest(socialGroup);
+						if (testprob <= TEST_PROBABILITY)
+						{
+							a.MakeTest();
+							tests++;
+							if (a.IsInfected())
+							{
+								for (int j = num; j < num + 4; j++)
+								{
+									agents[j].MakeTest();
+									tests++;
+								}
+							}
+						}
+					}
+					
+					std::uniform_int_distribution<> selfIsolationIndex(0, 100);
+					int index = selfIsolationIndex(socialGroup);
+					if (index <= SELF_ISOLATION_INGEX)
+					{
+						a.SetIsolation();
 					}
 				}
 				if (a.GetStage() == 't')
@@ -282,21 +416,108 @@ void SimulateInfection(vector<Agent> agents, const int nubmerOfAgents)
 					a.Death();
 					death++;
 				}
+				/*
+				if (a.GetStage() == 'r')
+				{
+					recovered++;
+				}
+				*/
 			}
 		}
 		//cout << "Day " << i << endl;
+		listOfInfected.push_back(counter);
+		listOfDeaths.push_back(death);
+		listOfRecovered.push_back(recovered);
+		listofTests.push_back(tests);
+		days.push_back(i);
 
 	}
 	cout << "Sick = " << counter << endl;
 	cout << "Death = " << death << endl;
+	cout << "Recovered = " << recovered << endl;
+	cout << "Tests = " << tests << endl;
+	//cout << "Data for indection plot"<< endl;
+	/*
+	for (auto c : days)
+	{
+		cout << c << ",";
+	}
+	cout << endl;
+	for (auto c : listOfInfected)
+	{
+		cout << c << ",";
+	}
+	cout << endl;
+
+	cout << "Data for death plot"<< endl;
+	for (auto c : days)
+	{
+		cout << c << ",";
+	}
+	cout << endl;
+	for (auto c : listOfDeaths)
+	{
+		cout << c << ",";
+	}
+	cout << endl;
+
+	cout << "Data for recovered plot" << endl;
+	for (auto c : days)
+	{
+		cout << c << ",";
+	}
+	cout << endl;
+	for (auto c : listOfRecovered)
+	{
+		cout << c << ",";
+	}
+	cout << endl;
+	*/
+	ofstream f;
+	f.open("data.csv");
+
+	vector<int> realData = {55,86,98,131,137,191,262,290,410,546,703,817,1014,1226,1613,1880,2475,2923,3357,3893,4484,5181,5841,6698,7822,8852,10158,11513,13002,14776,16146,18105,20754,24324,26350,29433,31981,33940,36897,39509,45351,48426,50646,53739,57300,62658,68606,74401,80115,85973,92676,98522,104189,109740,115909,121301,126004,130716,135464,138969,142824,146062,149607,152306,155219,158207,161397,163913,166473,169303,171443,173497,175829,178196,180791,183088,185374,187216,189214,191069,193061,195017,197018,198590,199785,201221,202935,204428,205905,207264 };
+
+	for (auto c : days)
+	{
+		f << c << ",";
+	}
+	f << endl;
+	for (auto c : days)
+	{
+		f << c << ",";
+	}
+	f << endl;
+	for (auto c : listOfInfected)
+	{
+		f << c << ",";
+	}
+	f << endl;
+	for (auto c : realData)
+	{
+		f << c << ",";
+	}
+	f << endl;
+	for (auto c : listOfDeaths)
+	{
+		f << c << ",";
+	}
+	f << endl;
+	for (auto c : listOfRecovered)
+	{
+		f << c << ",";
+	}
+
+
+	f.close();
 	//system("pause");
 
 }
 
 int main() 
 {
-	const int nubmerOfAgents = 100000;
-	const int countOfPatientZero = 10;
+	const int nubmerOfAgents = 12000000;
+	const int countOfPatientZero = 190;
 	vector<Agent> agents = MakeAgentsVecor(nubmerOfAgents);
 	for (int i = 0; i < countOfPatientZero; i++)
 	{
